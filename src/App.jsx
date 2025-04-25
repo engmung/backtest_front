735b3740d87d9f50f399591ccec9c65e549ca6e8
engmung <122682380+engmung@users.noreply.github.com>
2025-04-25 12:13:07 +0900

    sI

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { Analytics } from "@vercel/analytics/react";
-import { SpeedInsights } from "@vercel/speed-insights/next"; // <-- 1. Import SpeedInsights
+import { SpeedInsights } from "@vercel/speed-insights/react"; // <-- 1. Import SpeedInsights
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
